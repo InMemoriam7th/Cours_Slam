ace44b2a057677eb55c4403fb9146e7f0ac4f72a
InMemoriam7th <62008705+InMemoriam7th@users.noreply.github.com>
2020-09-10 17:20:57 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("chargé");
+
+
+    let el = document.querySelector(`ul`);
+    let machine_a_laver = {
+        marque: "brand",
+        prix: "399€",
+        consommation:  "700W/h" ,
+        matricule:  "Bxma37",
+        modele: 'asset/P955569-1.jpg',
+    }
+
+    let btn = document.querySelector("h1");
+    let cible = document.querySelector("html");
+
+    console.table(machine_a_laver);
+    console.log(machine_a_laver);
+    el.innerHTML+=`<li>Marque :${machine_a_laver.marque}</li>
+                <li>Prix :${machine_a_laver.prix}</li>
+                <li>Consommation :${machine_a_laver.consommation}</li>
+                <li>Matricule :${machine_a_laver.matricule}</li>
+                <li><img src="${machine_a_laver.modele}" alt="machine a laver"></img></li>
+                `
+
+    btn.addEventListener("click",(e)=>{
+        console.log("click");
+        cible.classList.toggle("access");
+    });  
+    
+    console.log(btn);
+}) 
